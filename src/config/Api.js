c3915eb42b61ddce3f98e7079b84c27001c09566
muhammadnurkholiq <mnurkholiq114@gmail.com>
2022-07-25 15:29:08 +0700

    bug api

--- src/config/Api.js
+++ src/config/Api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL:
+    process.env.SERVER_URL ||
     "https://dumbmerch-app-backend.herokuapp.com/api/v1" ||
     "http://localhost:5000/api/v1/",
 });
